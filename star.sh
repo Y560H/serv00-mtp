bb50e29fa961335d7449bfd8b19c1486c924b373
Y560H <y560h@outlook.com>
2025-02-18 09:54:28 +0800

    Add files via upload

---
+++ star.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+echo "==================================================================="
+echo "==================================================================="
+echo "  "
+echo "  "
+echo "  "
+echo "==================================================================="
+echo "==================================================================="
+
+
+mtproxy_script="https://raw.githubusercontent.com/Y560H/serv00-mtp/refs/heads/main/mtp.sh"
+
+
+
+select option in \
+
+    "安装 serv00 MTproxy" \
+    "退出"
+do
+    case $option in
+       
+        "安装 serv00 MTproxy")
+            echo "开始安装 MTproxy..."
+            bash <(curl -Ls "$mtproxy_script") || echo "Error: Failed to execute script."
+            ;;
+        "exit")
+            echo "Exiting the program"
+            break
+            ;;
+        *)
+            echo "Invalid option. Please try again."
+            ;;
+    esac
+done
